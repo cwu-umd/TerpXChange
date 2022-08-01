e796b08119db3fd0a97086def8974c4e62bf20f4
Hank Wu <hank@Hanks-MacBook-Air.local>
2022-07-31 22:44:43 -0400

    login authentication and navigation

--- TerpXChange/TerpXChange/LoginPage.swift
+++ TerpXChange/TerpXChange/LoginPage.swift
@@ -18,6 +18,8 @@ struct LoginPage: View {
     
     @State var errLabel: String = ""
     
+    @State var logginSuccess: Bool = false
+    
     
     var iconSize: CGFloat = UIScreen.main.bounds.width * 0.16
     var titleSize: CGFloat = UIScreen.main.bounds.width * 0.103
@@ -28,77 +30,89 @@ struct LoginPage: View {
     
     var body: some View {
         
-        VStack{
-            
-            Spacer()
+        NavigationView {
+        
+            VStack{
+                
+                Spacer()
 
-            HStack {
-                Text("TerpXChange")
-                    .font(.system(size: titleSize, weight: .bold))
-                    .italic()
-                
-                Image("origami")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: iconSize, height: iconSize)
-            }
-            
-            
-            TextField("Username", text: $username)
-                .padding(EdgeInsets(top: 0, leading: 25, bottom: 10, trailing: 0))
-            
-            
-            // Using padding instead of frame to maintain size across different screen
-            Divider().padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
-            
-            HStack {
-                
-                if !self.pwHidden {
-                    TextField("Password", text: $password)
-                        .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
-                } else {
-                    SecureField("Password", text: $password)
-                        .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
+                HStack {
+                    Text("TerpXChange")
+                        .font(.system(size: titleSize, weight: .bold))
+                        .italic()
+                    
+                    Image("origami")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: iconSize, height: iconSize)
                 }
+                
+                
+                TextField("Username", text: $username)
+                    .padding(EdgeInsets(top: 0, leading: 25, bottom: 10, trailing: 0))
+                
+                
+                // Using padding instead of frame to maintain size across different screen
+                Divider().padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+                
+                HStack {
+                    
+                    if !self.pwHidden {
+                        TextField("Password", text: $password)
+                            .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
+                    } else {
+                        SecureField("Password", text: $password)
+                            .padding(EdgeInsets(top: 0, leading: 25, bottom: 0, trailing: 0))
+                    }
 
-                
-                Button (action: {self.pwHidden.toggle()}) {
-                    Image (systemName: self.pwHidden ? "eye.slash.fill" : "eye.fill")
-                        .foregroundColor(self.pwHidden ? Color.secondary : Color.blue)
-                }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 25))
-                
-            }
-            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
-            
-            Divider().padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
-            
-//          Error message to be shown here (TBI)
-            if errLabel != "" {
-                Text(errLabel).padding(.all, 10.0).foregroundColor(.red)
-            }
-            
-            Button(action: {signIn(email: username, passwd: password)}){
-                Text("Sign In")
-                    .font(.system(size: newSignUp))
-                    .frame(width: signinButtonW, height: signinButtonH, alignment: .center)
-                    .overlay(Capsule().stroke().fill(Color.black))
-                    .foregroundColor(.black)
-            }.padding(.top, 5.0)
-            
-            Spacer()
-            
-            HStack {
-                Text("New to TerpXChange? ").font(.system(size: newSignUp))
-                
-                Button (action: {}){
-                    Text("Sign Up").font(.system(size: newSignUp))
+                    
+                    Button (action: {self.pwHidden.toggle()}) {
+                        Image (systemName: self.pwHidden ? "eye.slash.fill" : "eye.fill")
+                            .foregroundColor(self.pwHidden ? Color.secondary : Color.blue)
+                    }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 25))
+                    
                 }
+                .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                
+                Divider().padding(EdgeInsets(top: 0, leading: 20, bottom: 5, trailing: 20))
+                
+    //          Error message to be shown here (TBI)
+                if errLabel != "" {
+                    Text(errLabel).padding(.all, 10.0).foregroundColor(.red)
+                }
+                
+                Group {
+                    NavigationLink(destination: MainFeedPage().navigationBarBackButtonHidden(true).navigationTitle("").navigationBarHidden(true), isActive: $logginSuccess){
+                        Text("")
+                    }
+                    
+                    Button(action: {signIn(email: username, passwd: password)}){
+                        Text("Sign In")
+                            .font(.system(size: newSignUp))
+                            .frame(width: signinButtonW, height: signinButtonH, alignment: .center)
+                            .overlay(Capsule().stroke().fill(Color.black))
+                            .foregroundColor(.black)
+                    }.padding(.top, 5.0)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("New to TerpXChange? ").font(.system(size: newSignUp))
+                    
+                    Button (action: {}){
+                        Text("Sign Up").font(.system(size: newSignUp))
+                    }
+                }
+                .padding(.bottom)
+                
+                
             }
-            .padding(.bottom)
-            
+            .navigationBarHidden(true)
+            // End of V-Stack
             
         }
-//        End of V-Stack
+        // End of navigationview
         
         
     }
@@ -130,11 +144,14 @@ struct LoginPage: View {
                         errLabel = err!.localizedDescription
                     } else {
                         print("Welcome to TerpXChange")
+                        logginSuccess = true
                     }
                     
                 }
             }
             
+            // Testing purpose
+//            logginSuccess = true
             
             
         }
