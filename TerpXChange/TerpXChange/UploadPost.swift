fb5b8889e9c86a6ed835f3f7f3544a6245962a9e
Hank Wu <hank@hanks-air.myfiosgateway.com>
2023-08-11 17:29:53 -0400

    Upload post page UI

--- TerpXChange/TerpXChange/UploadPost.swift
+++ TerpXChange/TerpXChange/UploadPost.swift
@@ -18,62 +18,147 @@ struct UploadPost: View {
     @State var bookPrice = ""
     @State var bookDescription = "Book description..."
     
+    @State var showAlert = false
+    
+    @State var image: UIImage?
+//    = UIImage(systemName: "house")
+    @State var images: [UIImage] = []
+    
+    @State var showActionSheet: Bool = false
+    
+    @State var sourceType: UIImagePickerController.SourceType = .camera
+    @State var showImagePicker: Bool = false
+    
     var body: some View {
         
-        VStack {
-            
-            Group {
+        ScrollView {
+        
+            VStack {
                 
-                HStack {
+                Group {
                     
-                    Button(action: {}){
-                        Text("Cancel").padding()
-                    }
-                    
-                    Spacer()
-                    
-                    Button(action: {
+                    HStack {
                         
-                    }){
-                        Text("Post")
-                            .padding()
-                    }
-                }
-                .foregroundColor(.red)
-                .clipShape(Capsule())
-                
-            }
-            
-            Group {
-                Divider()
-                TextField("Book title (required)", text: $bookTitle)
-                Divider()
-                TextField("Class category", text: $bookCat)
-                Divider()
-                
-                HStack {
-                    TextField("Class name", text: $className)
-                    Divider()
-                    TextField("book price (require)", text: $bookPrice)
-                }.frame(height: 25)
-                
-                Divider()
-                
-                TextEditor(text: $bookDescription)
-                    .padding(.top, 2)
-                    .foregroundColor(bookDescription == "Book description..." ? .gray : .primary)
-                    .onTapGesture {
-                        if bookDescription == "Book description..." {
-                            bookDescription = ""
+                        Button(action: {}){
+                            Text("Cancel").padding()
+                        }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            showAlert.toggle()
+                        }){
+                            Text("Post")
+                                .padding()
+                                .alert(isPresented: $showAlert) {
+                                    Alert(title: Text("Unable to post"), message: Text("please fill in all required fields"), dismissButton: nil)
+                                }
                         }
                     }
+                    .foregroundColor(.red)
+                    .clipShape(Capsule())
+                    
+                }
+                
+                Group {
+                    Divider()
+                    TextField("Book title (required)", text: $bookTitle).padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                    Divider()
+                    TextField("Class category", text: $bookCat).padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                    Divider()
+                    
+                    HStack {
+                        TextField("Class name", text: $className).padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                        Divider()
+                        TextField("book price (require)", text: $bookPrice).padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+                    }.frame(height: 25)
+                    
+                    Divider()
+                    
+                    TextEditor(text: $bookDescription)
+                        .frame(width: UIScreen.main.bounds.size.width, height: 150)
+                        .onTapGesture {
+                            if self.bookDescription == "Book description..." {
+                                self.bookDescription = ""
+                            }
+                        }
+                    
+                    Divider()
+                    
+                    ScrollView (.horizontal, showsIndicators: false, content: {
+                        
+                        HStack (spacing: 15) {
+                            
+                            if image != nil {
+                                Image (uiImage: image!)
+                                    .resizable()
+                                    .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+                                    .frame(width: UIScreen.main.bounds.size.width, height: 250)
+                            } else {
+                                Text("No current image selected!")
+                                    .frame(width: UIScreen.main.bounds.size.width)
+                            }
+                            
+                            
+
+    //                        ForEach (images, id: \.self) { img in
+    //
+    //                            Image (uiImage: img)
+    //                                .resizable()
+    //                                .frame(width: UIScreen.main.bounds.width - 30, height: 250)
+    //                        }
+                            
+                        }
+                        
+                    })
+                    .frame(height: 250)
+                    
+                }
                 
                 Divider()
                 
+                Group {
+                    HStack {
+                        Text("Choose picture")
+                            .font(.caption)
+                        
+                        Spacer()
+                        
+                        Button(action: {showActionSheet.toggle()}) {
+                            Image(systemName: "camera")
+                        }
+                        .actionSheet(isPresented: $showActionSheet) {
+                            ActionSheet(title: Text("Add a picture to your post"), message: nil, buttons:[
+                                .default(Text("Camera"), action: {
+                                    showImagePicker.toggle()
+                                    sourceType = .camera
+                                }),
+
+                                .default(Text("Photo Library"), action: {
+                                    showImagePicker.toggle()
+                                    sourceType = .photoLibrary
+                                }),
+
+                                .cancel()
+
+                            ])
+                        }
+                        
+                        .sheet(isPresented: $showImagePicker) {
+                            imagePicker(image: $image, showImagePicker: $showImagePicker, sourceType: sourceType)
+                        }
+                        
+                        
+                    }
+    //                End of HStack
+                }
+    //            End of Group
+                
+                
+                
                 
             }
-            
-            
+//            End of VStack
             
         }
         
@@ -83,6 +168,7 @@ struct UploadPost: View {
 
 struct uploadpost_preview: PreviewProvider {
     static var previews: some View {
+        UploadPost().previewDevice("iPhone 8")
         UploadPost().previewDevice("iPhone 12")
     }
 }
